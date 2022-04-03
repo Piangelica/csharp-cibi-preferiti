60716563a472722d59170c3b273f97176448735c
PC <PC@DESKTOP-LUEVN40>
2022-04-03 14:37:21 +0200

    Aggiungere i file di progetto.

---
+++ es/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi). 
+ L’array deve essere già inizializzato nel programma, e i vostri cibi preferiti non vanno chiesti all’utente tramite input.
+Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
+-La lunghezza della classifica
+-La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+-Il vostro cibo top (prima posizione della classifica)
+-Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+Le modalità di consegna sono le solite: repo GitHub, commit e push*/
+
+string[] cibi = new string[5];
+cibi[0] = "Crêpes";
+cibi[1] = "Pizza";
+cibi[2] = "Cioccolato";
+cibi[3] = "Croissants";
+cibi[4] = "Riso";
+
+for (int i = 0; i < cibi.Length; i++)
+{
+    Console.WriteLine((i + 1) + ")" + cibi[i]);
+}
+
+var primocibo = cibi[0];
+Console.WriteLine("Ecco il mio cibo preferito:" + primocibo);
+var ultimocibo = cibi[4];
+Console.WriteLine("Ecco il mio cibo preferito ma non troppo:" + cibi[4]);
